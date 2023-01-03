4ca7cc941fcf48508cdd3efaed92371d2851dcf5
Gabriel Almeida <113075816+1neskk@users.noreply.github.com>
2023-01-03 20:43:38 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
                 <div className="flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6">
                   <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 ">
                     <img src="https://modernhoobank.netlify.app/assets/Discount.61d9dc08.svg" alt="discount" className="w-[32px] h-[32px]" />
-                    <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2">
+                    <p className="main__text font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2">
                       <span className="text-white">20% </span>
                       Discount For
                       <span className="text-white"> 1 Months </span>
